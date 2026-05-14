d78855931d9d6003b406f0c0a2fd666ea0a222bf
Xiangovob <sandy920321@gmail.com>
2026-05-14 23:28:59 +0800

    Update game.js

--- game.js
+++ game.js
@@ -349,54 +349,6 @@ function useCombatCard(card, owner){
     doCombat(player, enemy);
 }
 
-function checkDeaths(who){
-    if(who.combat && who.combat.currentHp <= 0){
-        killUnit(who.combat, who);
-        who.combat = null;
-    }
-
-    who.bench.forEach(unit => {
-        if(unit.currentHp <= 0 && !unit.isDead){
-            killUnit(unit, who);
-        }
-    });
-}
-
-function killUnit(unit, who){
-    unit.currentHp = 0;
-    unit.shield = 0;
-    unit.isDead = true;
-    unit.reviveCounter = 3;
-
-    let enemySide = who === player ? enemy : player;
-    triggerPassive(unit, "onDeath", who, enemySide);
-
-    if(who.combat === unit){
-        who.combat = null;
-    }
-
-    addLog(`${unit.name}死亡，進入準備區，3回合後復活`);
-}
-
-function reviveCharacters(who){
-    who.bench.forEach(unit => {
-        if(unit.isDead){
-            unit.reviveCounter--;
-
-            addLog(`${unit.name}復活倒數：${unit.reviveCounter}`);
-
-            if(unit.reviveCounter <= 0){
-                unit.isDead = false;
-                unit.currentHp = unit.hp;
-                unit.shield = 0;
-                unit.reviveCounter = 0;
-
-                addLog(`${unit.name}復活，回到${unit.currentHp}血`);
-            }
-        }
-    });
-}
-
 function surrender(){
     player.coreHp = 0;
     addLog("你選擇投降");

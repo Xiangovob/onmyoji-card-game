bc5b4c2f091fbcf4d3494b10c92e16f3b75f3b19
Xiangovob <sandy920321@gmail.com>
2026-05-14 18:46:18 +0800

    Update combat.js

--- combat.js
+++ combat.js
@@ -33,3 +33,30 @@ function dealDamage(unit, damage){
         triggerPassive(unit, "onHurt", ownerSide, enemySide);
     }
 }
+
+function healTarget(unit, amount){
+    unit.currentHp += amount;
+
+    if(unit.currentHp > unit.hp){
+        unit.currentHp = unit.hp;
+    }
+}
+
+function doCombat(attackerSide, defenderSide){
+    let attacker = attackerSide.combat;
+    let defender = defenderSide.combat;
+
+    if(!attacker || attacker.isDead) return;
+
+    triggerPassive(attacker, "onAttack", attackerSide, defenderSide);
+
+    if(defender && !defender.isDead){
+        dealDamage(defender, attacker.atk);
+        dealDamage(attacker, defender.atk);
+
+        addLog(`${attacker.name}與${defender.name}交戰`);
+    }else{
+        defenderSide.coreHp -= attacker.atk;
+        addLog(`${attacker.name}攻擊${defenderSide.name}本體，造成${attacker.atk}傷害`);
+    }
+}

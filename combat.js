afc066f91842020ea11a81287500aebcbd6ce94d
Xiangovob <sandy920321@gmail.com>
2026-05-14 18:44:26 +0800

    Update combat.js

--- combat.js
+++ combat.js
@@ -1,1 +1,13 @@
+function getAtk(unit){
 
+    let baseAtk =
+        unit.form
+        ? unit.form.atk
+        : unit.atk;
+
+    return (
+        baseAtk +
+        unit.buffAtk -
+        unit.debuffAtk
+    );
+}

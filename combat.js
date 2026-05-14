b5b2aa2f66cea7a5fc750a1369b38d54d58e25b4
Xiangovob <sandy920321@gmail.com>
2026-05-14 18:44:47 +0800

    Update combat.js

--- combat.js
+++ combat.js
@@ -11,3 +11,25 @@ function getAtk(unit){
         unit.debuffAtk
     );
 }
+
+function dealDamage(unit, damage){
+    if(unit.shield > 0){
+        let block = Math.min(unit.shield, damage);
+        unit.shield -= block;
+        damage -= block;
+        addLog(`${unit.name}護盾抵擋${block}傷害`);
+    }
+
+    if(damage > 0){
+        unit.currentHp -= damage;
+
+        if(unit.currentHp < 0){
+            unit.currentHp = 0;
+        }
+
+        let ownerSide = player.bench.includes(unit) ? player : enemy;
+        let enemySide = ownerSide === player ? enemy : player;
+
+        triggerPassive(unit, "onHurt", ownerSide, enemySide);
+    }
+}

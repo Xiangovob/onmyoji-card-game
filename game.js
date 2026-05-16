97a5edb40548567c24c2eeeb7026424da8ec2922
Xiangovob <sandy920321@gmail.com>
2026-05-17 07:50:48 +0800

    Update game.js

--- game.js
+++ game.js
@@ -212,30 +212,6 @@ function useCombatCard(card, owner){
     addLog(
         `${owner.name}使用戰鬥牌：${card.name}並出戰`
     );
-    if(card.damage){
-        owner.combatAtk += card.damage;
-        addLog(
-            `${owner.name}攻擊增加${card.damage}`
-        );
-    }
-    if(card.shield){
-        owner.shield += card.shield;
-        addLog(
-            `${owner.name}獲得${card.shield}護盾`
-        );
-    }
-    if(card.immuneCombat){
-        owner.immuneCombat = true;
-        addLog(
-            `${owner.name}免疫本次戰鬥傷害`
-        );
-    }
-    if(card.drawOnKill){
-        owner.drawOnKill = true;
-        addLog(
-            `${owner.name}本次戰鬥擊殺敵人時將抽1張牌`
-        );
-    }
     doCombat(player, enemy);
 }
 function surrender(){

16528fe1dbdf5d4741408f6c2db1a7c306e6fcb7
Xiangovob <sandy920321@gmail.com>
2026-05-17 07:48:19 +0800

    Update combatEffects.js

--- combatEffects.js
+++ combatEffects.js
@@ -1,1 +1,29 @@
+function applyCombatEffects(card, owner){
+    if(card.damage){
+        owner.combatAtk += card.damage;
+        addLog(
+            `${owner.name}攻擊增加${card.damage}`
+        );
+    }
 
+    if(card.shield){
+        owner.shield += card.shield;
+        addLog(
+            `${owner.name}獲得${card.shield}護盾`
+        );
+    }
+
+    if(card.immuneCombat){
+        owner.immuneCombat = true;
+        addLog(
+            `${owner.name}免疫本次戰鬥傷害`
+        );
+    }
+
+    if(card.drawOnKill){
+        owner.drawOnKill = true;
+        addLog(
+            `${owner.name}本次戰鬥擊殺敵人時將抽1張牌`
+        );
+    }
+}

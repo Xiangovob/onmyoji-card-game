6e8b74b091c3eed9abcd3bbf2ac45cc37c2fce7e
Xiangovob <sandy920321@gmail.com>
2026-05-20 20:13:30 +0800

    Update effects.js

--- effects.js
+++ effects.js
@@ -54,6 +54,7 @@ function isValidSpellTarget(card, sideName, targetType, target){
 }
 
 function useSpellCard(card, owner, targetInfo){
+
     let ownerSide =
         player.bench.includes(owner)
         ? player
@@ -64,6 +65,9 @@ function useSpellCard(card, owner, targetInfo){
         ? enemy
         : player;
 
+    let target = targetInfo.target;
+
+    // AOE
     if(card.tags.includes("aoe")){
 
         let targets = getAllAliveEnemyUnits();
@@ -75,49 +79,108 @@ function useSpellCard(card, owner, targetInfo){
         addLog(
             `${owner.name}使用${card.name}，對敵方全體造成${card.damage}傷害`
         );
+
         triggerPassive(
             owner,
             "onSpell",
             ownerSide,
             enemySide
         );
-        
+
         return;
     }
+
+    // 所有敵方目標
     if(card.tags.includes("allEnemy")){
+
         // 敵方戰鬥區
         if(enemySide.combat && !enemySide.combat.isDead){
-            dealDamage(enemySide.combat, card.damage);
+
+            dealDamage(
+                enemySide.combat,
+                card.damage
+            );
         }
+
         // 敵方準備區
         enemySide.bench.forEach(unit => {
-    
+
             if(!unit.isDead){
-    
-                dealDamage(unit, card.damage);
+
+                dealDamage(
+                    unit,
+                    card.damage
+                );
             }
         });
+
         // 敵方本體
         enemySide.coreHp -= card.damage;
+
         addLog(
             `${owner.name}使用${card.name}，對所有敵方目標造成${card.damage}傷害`
         );
+
         triggerPassive(
             owner,
             "onSpell",
             ownerSide,
             enemySide
         );
+
         return;
     }
 
-    let target = targetInfo.target;
+    // 檢索
+    if(card.tags.includes("search")){
 
+        let pool = ownerSide.deck.filter(
+            c => c.owner === target.id
+        );
+
+        if(pool.length <= 0){
+
+            addLog(
+                `${target.name}已沒有專屬牌`
+            );
+
+            return;
+        }
+
+        let random =
+            pool[
+                Math.floor(
+                    Math.random() * pool.length
+                )
+            ];
+
+        ownerSide.hand.push(random);
+
+        ownerSide.deck.splice(
+            ownerSide.deck.indexOf(random),
+            1
+        );
+
+        addLog(
+            `${owner.name}使用${card.name}，抽出了${target.name}的專屬牌`
+        );
+
+        triggerPassive(
+            owner,
+            "onSpell",
+            ownerSide,
+            enemySide
+        );
+
+        return;
+    }
+
+    // 傷害
     if(card.tags.includes("damage")){
 
         if(targetInfo.targetType === "core"){
 
-            enemy.coreHp -= card.damage;
+            enemySide.coreHp -= card.damage;
 
             addLog(
                 `${owner.name}使用${card.name}，對敵方本體造成${card.damage}傷害`
@@ -125,7 +188,10 @@ function useSpellCard(card, owner, targetInfo){
 
         }else{
 
-            dealDamage(target, card.damage);
+            dealDamage(
+                target,
+                card.damage
+            );
 
             addLog(
                 `${owner.name}使用${card.name}，對${target.name}造成${card.damage}傷害`
@@ -133,6 +199,7 @@ function useSpellCard(card, owner, targetInfo){
         }
     }
 
+    // 治療
     if(card.tags.includes("heal")){
 
         healTarget(
@@ -148,6 +215,7 @@ function useSpellCard(card, owner, targetInfo){
         );
     }
 
+    // 護盾
     if(card.tags.includes("shield")){
 
         target.shield += card.shield;
@@ -156,6 +224,7 @@ function useSpellCard(card, owner, targetInfo){
             `${owner.name}使用${card.name}，${target.name}獲得${card.shield}護盾`
         );
     }
+
     triggerPassive(
         owner,
         "onSpell",
@@ -163,7 +232,6 @@ function useSpellCard(card, owner, targetInfo){
         enemySide
     );
 }
-
 function getAllAliveEnemyUnits(){
 
     let set = new Set();

0d05e00b125425e2134412656344919ab50071f3
Xiangovob <sandy920321@gmail.com>
2026-05-21 00:55:58 +0800

    Create decks.js

---
+++ decks.js
@@ -0,0 +1,35 @@
+const deckData = {
+
+    starterDeck:[
+
+        "holyLight",
+        "holyLight",
+
+        "holySpring",
+        "holySpring",
+
+        "foxFire",
+        "foxFire",
+
+        "foxCry",
+        "foxCry"
+    ],
+
+    priestDeck:[
+
+        "holyLight",
+        "holyLight",
+
+        "holySpring",
+        "holySpring"
+    ],
+
+    foxDeck:[
+
+        "foxFire",
+        "foxFire",
+
+        "foxCry",
+        "foxCry"
+    ]
+};

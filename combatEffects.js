58c6b663b11ae5ef771eaf4d02a147ef965587b2
Xiangovob <sandy920321@gmail.com>
2026-05-25 01:29:51 +0800

    Update combatEffects.js

--- combatEffects.js
+++ combatEffects.js
@@ -10,7 +10,6 @@ function applyCombatEffects(card, owner){
             `${owner.name}攻擊增加${card.damage}`
         );
     }
-
     // 護盾
     if(card.shield){
         owner.shield += card.shield;
@@ -18,7 +17,6 @@ function applyCombatEffects(card, owner){
             `${owner.name}獲得${card.shield}護盾`
         );
     }
-
     // 免疫戰鬥傷害
     if(card.immuneCombat){
         addStatus(owner, {
@@ -29,7 +27,6 @@ function applyCombatEffects(card, owner){
             `${owner.name}免疫本次戰鬥傷害`
         );
     }
-
     // 擊殺抽牌
     if(card.drawOnKill){
         owner.drawOnKill = true;
@@ -37,20 +34,15 @@ function applyCombatEffects(card, owner){
             `${owner.name}本次戰鬥擊殺敵人時將抽1張牌`
         );
     }
+    // 護盾轉攻擊
     if(card.tags.includes("shieldScale")){
-        owner.tempAtk =
-            (owner.tempAtk || 0)
-            + owner.shield;
+        addStatus(owner, {
+            type: "atkUp",
+            value: owner.shield,
+            duration: 1
+        });
         addLog(
             `${owner.name}因護盾獲得${owner.shield}攻擊`
         );
     }
-
-    if(card.tags.includes("shieldScale")){
-        owner.combatAtk += owner.shield;
-        addLog(
-            `${owner.name}因護盾獲得${owner.shield}攻擊`
-        );
-    }
-    
 }
